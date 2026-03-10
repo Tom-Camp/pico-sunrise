78650c366dc5837135cbaab9804dd4992b780c1b
Tom Camp <tom.camp@civicactions.com>
2026-03-10 10:16:49 -0400

    Updating wake cycle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import machine
 import neopixel
+import time
 
 import secrets
 from lib.sunrise import Sunrise
@@ -8,15 +9,24 @@ from lib.wifi_manager import WiFiManager
 
 wifi = WiFiManager(ssid=secrets.SSID, password=secrets.PASSWORD)
 
-sync_and_set_rtc()
 
-if machine.reset_cause() == machine.DEEPSLEEP_RESET:
-    light_pin = machine.Pin(18)
-    sunrise = Sunrise(pixels=neopixel.NeoPixel(light_pin, 32))
+light_pin = machine.Pin(28)
+sunrise = Sunrise(pixels=neopixel.NeoPixel(light_pin, 32))
+
+while True:
     try:
+        sync_and_set_rtc()
+        sleep_time = next_alarm()
+        # Wake an hour before the alarm
+        time.sleep(sleep_time - 3600)
+        sync_and_set_rtc()
+        # Set wake time
+        wake_time = next_alarm()
+        time.sleep(wake_time)
         sunrise.sunrise()
     except KeyboardInterrupt:
         sunrise.stop()
-
-sleep_time = next_alarm()
-machine.deepsleep(sleep_time * 1000)
+    except Exception as e:
+        sunrise.stop()
+        time.sleep(5)
+        machine.reset()
